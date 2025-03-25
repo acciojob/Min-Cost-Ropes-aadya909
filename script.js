2e5c404b424900f778b66a7293e34786bc9aedf6
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-25 11:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function mincost(arr)
-{ 
-//write your code here
-// return the min cost
+{ if(arr.length===1)return 0;
+ arr.sort((a,b)=>a-b);
+ let totalcost=0;
+while(arr.length>1){
+	let first=arr.shift();
+	let second=arr.shift();
+	let cost=first+second;
+	totalcost+=cost;
+	let i=0;
+	while(i<arr.length && arr[i]<cost){
+		i++;
+	}
+	arr.splice(i,0,cost);
+}
+ return  totalcost;
   
 }
 
